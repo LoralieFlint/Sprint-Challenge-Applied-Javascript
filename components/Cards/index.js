30e2316d21efcc858b92e58b2193cbb35c2c24d0
ittyBittyBytes <loralie.flint0128@gmail.com>
2019-08-17 12:42:12 -0600

    MVP was met and questions are inswered in tabs.js

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -1,24 +1,66 @@
-// creating component
-//     /* creating component */
-// function cards() {
-//     /*  elements*/
-//     const div1 = document.createElement("div");
-//     const div2 = document.createElement("div");
-//     const div3 = document.createElement("div");
-//     const div4 = document.createElement("div");
-//     const image = document.createElement("img");
-//     const span = document.createElement("span");
-    
-//     /* adding classes and textContent */
-//     div1.classList.add("card");
-//     div2.classList.add("headline");
-//     div3.classList.add("author");
-//     div4.classList.add("img-container");
-//     div2.textContent = "headline"
-//     image.src = imageUrl;
-//     span.textContent = `By ${name}`;
-    
+/* creating component */
+function cards(article) {
+  /*  elements*/
+  const div1 = document.createElement("div");
+  const div2 = document.createElement("div");
+  const div3 = document.createElement("div");
+  const div4 = document.createElement("div");
+  const image = document.createElement("img");
+  const span = document.createElement("span");
 
-//     /* returning the component */
-//     return div1;
-//     }
+  /* adding classes and textContent */
+  div1.classList.add("card");
+  div2.classList.add("headline");
+  div3.classList.add("author");
+  div4.classList.add("img-container");
+  div2.textContent = article.headline;
+  image.src = article.authorPhoto;
+  span.textContent = `By ${article.authorName}`;
+
+  /* appending children */
+  div1.appendChild(div2);
+  div1.appendChild(div3);
+  div3.appendChild(div4);
+  div4.appendChild(image);
+  div3.appendChild(span);
+
+/* Create a card for each of the articles and add the card to the DOM. */
+const JScard = document.querySelector(".tab").addEventListener("click", (event) =>{
+    
+}) 
+//     const card2 = cards();
+//     const card3 = cards();
+//     const card4 = cards();
+//     const card5 = cards();
+
+
+/* appending cards to cards-container class. */
+// const cardContainer = document.querySelector(".tab");
+// cardContainer.appendChild(JScard);
+// cardContainer.appendChild(card2);
+// cardContainer.appendChild(card3);
+// cardContainer.appendChild(card4);
+// cardContainer.appendChild(card5);
+
+  /* returning the component */
+  return div1;
+}
+const cardItem = document.querySelector('.cards-container');
+axios
+  .get('https://lambda-times-backend.herokuapp.com/articles')
+  .then(response => {
+    console.log(response);
+    Object.values(response.data.articles).forEach(item => {
+      item.forEach(item => {
+        let Cards = document.querySelector('.cards-container');
+        Cards.appendChild(cards(item));
+      })
+    })
+  })
+  .catch(error => {
+    console.log(error);
+  })
+
+
+
+
--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -1,3 +1,20 @@
+/*1. What is the DOM? */
+/* DOM stands for document object model where u can create your page dynamically */
+
+/* 2. What is an event? */
+/* an event is an action that happens with user interaction */
+
+/* 3. What is an event listener? */
+/* an event listener is a function waiting to happen on a certain call that is made by the user */
+
+/* 4. Why would we convert a NodeList into an Array? */
+/* one example for  a node list may be used in an array to call certain items and use them dynamically on a page */
+
+/* 5. What is a component? */
+/* a component is a creator function which can be reusable such as creating elements and click event to make it easier to read and easier to use over and over */
+
+
+
 /* creating component */
 function tabs(topics) {
     /* creating element */
@@ -9,7 +26,7 @@ function tabs(topics) {
     return div;
   }
   /* appending component to correct class*/
-  const tabsContainer = document.querySelector(".topics").appendChild(tabs());
+  const tabsContainer = document.querySelector(".topics");
   
 /* calling axious */
   axios
